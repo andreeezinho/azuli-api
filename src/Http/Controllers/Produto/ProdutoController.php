1dcc934346c633cf585a244c5a3b1f0251f328f2
andreeezinho <andreeezinho@gmail.com>
2026-07-09 21:18:51 -0300

    feat: update store product method

--- src/Http/Controllers/Produto/ProdutoController.php
+++ src/Http/Controllers/Produto/ProdutoController.php
@@ -5,16 +5,39 @@ namespace App\Http\Controllers\Produto;
 use App\Http\Controllers\Controller;
 use App\Http\Request\Request;
 use App\Domain\Repositories\Produto\ProdutoRepositoryInterface;
+use App\Domain\Repositories\GrupoProduto\GrupoProdutoRepositoryInterface;
+use App\Domain\Repositories\Tributacao\IcmsRepositoryInterface;
+use App\Domain\Repositories\Tributacao\IpiRepositoryInterface;
+use App\Domain\Repositories\Tributacao\PisRepositoryInterface;
+use App\Domain\Repositories\Tributacao\CofinsRepositoryInterface;
 use App\Http\Transformer\Produto\ProdutoTransformer;
 
 class ProdutoController extends Controller {
 
     protected $produtoRepository;
+    protected $grupoProdutoRepository;
+    protected $icmsRepository;
+    protected $ipiRepository;
+    protected $pisRepository;
+    protected $cofinsRepository;
     protected $produtoTransformer;
 
-    public function __construct(ProdutoRepositoryInterface $produtoRepository, ProdutoTransformer $produtoTransformer){
+    public function __construct(
+        ProdutoRepositoryInterface $produtoRepository, 
+        GrupoProdutoRepositoryInterface $grupoProdutoRepository, 
+        IcmsRepositoryInterface $icmsRepository,
+        IpiRepositoryInterface $ipiRepository,
+        PisRepositoryInterface $pisRepository,
+        CofinsRepositoryInterface $cofinsRepository,
+        ProdutoTransformer $produtoTransformer
+    ){
         parent::__construct();
         $this->produtoRepository = $produtoRepository;
+        $this->grupoProdutoRepository = $grupoProdutoRepository;
+        $this->icmsRepository = $icmsRepository;
+        $this->ipiRepository = $ipiRepository;
+        $this->pisRepository = $pisRepository;
+        $this->cofinsRepository = $cofinsRepository;
         $this->produtoTransformer = $produtoTransformer;
     }
 
@@ -32,6 +55,12 @@ class ProdutoController extends Controller {
     public function store(Request $request){
         $data = $request->all();
 
+        if($this->produtoRepository->findBy('codigo', $data['codigo']) != null){
+            return $this->respJson([
+                'message' => 'Código de produto já existente'
+            ], 422);
+        }
+
         $validate = $this->validate($data, [
             'nome' => 'required|string|max:100',
             'codigo' => 'required|max:13',
@@ -40,11 +69,6 @@ class ProdutoController extends Controller {
             'tipo' => 'required|string',
             'quant_entrada' => 'required|float',
             'quant_saida' => 'required|float',
-            'grupo_produto_id' => 'required|int',
-            'icms_id' => 'required|int',
-            'ipi_id' => 'required|int',
-            'pis_id' => 'required|int',
-            'cofins_id' => 'required|int',
             'cfop' => 'required|int',
             'ncm' => 'int',
             'ativo' => 'max:1'
@@ -56,6 +80,12 @@ class ProdutoController extends Controller {
                 'errors' => $this->getErrors()
             ], 422);
         }
+
+        $data['grupo_produto_id'] = $this->grupoProdutoRepository->findBy('uuid', $data['grupo_produto_id'])->id ?? 1;
+        $data['icms_id'] = $this->icmsRepository->findBy('uuid', $data['icms_id'])->id ?? null;
+        $data['ipi_id'] = $this->ipiRepository->findBy('uuid', $data['ipi_id'])->id ?? null;
+        $data['pis_id'] = $this->pisRepository->findBy('uuid', $data['pis_id'])->id ?? null;
+        $data['cofins_id'] = $this->cofinsRepository->findBy('uuid', $data['cofins_id'])->id ?? null;
 
         $produto = $this->produtoRepository->create($data);
 
